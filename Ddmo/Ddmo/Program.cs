0ea0ee9bad53699fea84d9728ca493c78b5d6f76
000 <1875680955@qq.com>
2019-03-18 16:38:42 +0800

    test

---
+++ Ddmo/Ddmo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ddmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///测试
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+        }
+    }
+}
