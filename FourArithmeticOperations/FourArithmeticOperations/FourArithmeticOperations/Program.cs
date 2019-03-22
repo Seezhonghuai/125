4680d7729aa3be2d849658fc2334893c68450979
1875680955@qq.com <1875680955@qq.com>
2019-03-22 21:00:12 +0800

    测试

--- FourArithmeticOperations/FourArithmeticOperations/FourArithmeticOperations/Program.cs
+++ FourArithmeticOperations/FourArithmeticOperations/FourArithmeticOperations/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace FourArithmeticOperations
 {
-    class Program
+   public class Program
     {    
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             Console.WriteLine("随机生成10道四则运算");
             Console.ReadLine();
