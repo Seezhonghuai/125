4662d4699e46edf5fd3f4927dcce50141fb096f2
1875680955@qq.com <1875680955@qq.com>
2019-03-22 20:10:51 +0800

    整理规范代码

--- FourArithmeticOperations/FourArithmeticOperations/FourArithmeticOperations/Program.cs
+++ FourArithmeticOperations/FourArithmeticOperations/FourArithmeticOperations/Program.cs
@@ -7,11 +7,7 @@ using System.Threading.Tasks;
 namespace FourArithmeticOperations
 {
     class Program
-    {
-        /// <summary>
-        /// FourArithmeticOperations
-        /// </summary>
-        /// <param name="args"></param>
+    {    
         static void Main(string[] args)
         {
             Console.WriteLine("随机生成10道四则运算");
@@ -22,10 +18,8 @@ namespace FourArithmeticOperations
             {
                 int num1 = number.Next(1, 10);//随机一个10以内的数。
                 int num2 = number.Next(1, 10);//随机一个10以内的数。
-
                 int opnext = number.Next(0, 4);//随机抽取一个 "＋", "－", "*", "÷" 符号题目呀
                 int num3 = number.Next(2, 10);
-
                 int s = 0;
                 s = Math.Abs(s);//绝对值函数
                 if (num1 < num2)//如果左边的数小于右边的数就置换过来，避免负数的出现。
