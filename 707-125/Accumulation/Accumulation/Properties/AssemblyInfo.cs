be6ecf809a3b7556431c3c8f4da93fbc6a66bccc
000 <1875680955@qq.com>
2019-04-15 15:49:19 +0800

    添加Accumulation

--- 707-125/Accumulation/Accumulation/Properties/AssemblyInfo.cs
+++ 707-125/Accumulation/Accumulation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Ddmo")]
+[assembly: AssemblyTitle("Accumulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ddmo")]
+[assembly: AssemblyProduct("Accumulation")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("ba53bacb-a928-4991-9fe7-80389b01868a")]
+[assembly: Guid("9833f75d-e99f-4a17-af11-52f8d9d9d25b")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
